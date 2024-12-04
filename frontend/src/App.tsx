34e1d5e4f15f81425fa7f311ccf35f56b2dbb19f
Antoine Debroye <antoine.debroye@intersec.com>
2024-12-04 21:20:24 +0100

    feat: Change the react default app page to my new one

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,30 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { RetroProvider } from "./contexts/RetroContext";
+import Home from "./pages/Home";
+import Retro from "./pages/Retro";
+import Header from "./components/Header";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RetroProvider>
+      <Router>
+        <Header />
+        <div className="container mx-auto p-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/retro/:id" element={<Retro />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
+      </Router>
+    </RetroProvider>
   );
-}
+};
 
 export default App;
